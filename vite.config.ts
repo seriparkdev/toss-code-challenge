c8217653fa9e2beb183da621ddb112c0f428078e
seriparkdev <parkseridev@gmail.com>
2025-09-20 19:53:25 +0900

    chore: 환경세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({});
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+})
+
